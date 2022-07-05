8f6be77fbf189ee8a07f5e0d5d82aec87493d9a2
MarcoDouglas <marcomcd2@gmail.com>
2022-07-05 10:43:49 -0300

    :sparkles: feat: Formulário de alteração

--- src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -33,6 +33,15 @@ public class Banco {
 			}
 		}	
 	}
+
+	public Empresa buscaEmpresaPelaId(Integer id) {
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}	
+		return null;
+	}
 	
 	public List<Empresa> getEmpresas(){
 		return Banco.lista;
